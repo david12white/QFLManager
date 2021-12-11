fc8b22d68c17aec6d77da823b46e78b10c06d488
david <david12white@gmail.com>
2021-12-10 22:38:11 -0500

    Displaying Player.

--- Quad Football Manager-core/src/com/mygdx/game/QFLManager.java
+++ Quad Football Manager-core/src/com/mygdx/game/QFLManager.java
@@ -8,11 +8,13 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class QFLManager extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture redPlayer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		redPlayer = new Texture("red_player.png");
 	}
 
 	@Override
@@ -20,6 +22,7 @@ public class QFLManager extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(redPlayer, 200, 200);
 		batch.end();
 	}
 	
